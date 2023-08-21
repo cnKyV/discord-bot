4ad46f73253d9a79c6d5080329ccef2742d8a9c3
Cenkarof <vergilicenkay@gmail.com>
2023-08-21 22:02:21 +0300

    converting index.js to support multiple files

--- src/index.js
+++ src/index.js
@@ -1,31 +1,76 @@
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
-const {REST, Routes, Client, GatewayIntentBits} = require('discord.js');
+const {REST, Routes, Events, Client, GatewayIntentBits, Collection} = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
 client.commands = new Collection();
 
-const commandPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const file of commandFiles)
+console.log(`foldersPath: ${foldersPath} commandFolders: ${commandFolders}`);
+
+for (const folder of commandFolders)
 {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-    if('data' in command && 'execute' in command)
+    console.log(`commandsPath: ${commandsPath} commandFiles: ${commandFiles}`);
+
+    for(const file of commandFiles)
     {
-        client.commands.set(command.data.name, command);
-        console.log(`Loaded command ${command.data.name} from ${filePath}`);
-    }
-    else
-    {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        console.log(`filePath: ${filePath} command: ${command}`);
+
+        if('data' in command && 'execute' in command)
+            client.commands.set(command.data.name, command);
+            
+        
+        else
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+
+        console.log(client.commands);
     }
 
 }
 
+
+client.on(Events.InteractionCreate, async interaction => 
+    {
+        if(!interaction.isChatInputCommand()) 
+            return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if(!command)
+        {
+            console.error(`[ERROR] Command ${interaction.commandName} not found.`);      
+            return;
+        }
+
+        
+        try 
+        {
+            await command.execute(interaction);
+        }
+        catch (error)
+        {
+            console.error(`[ERROR] Failed to execute command ${interaction.commandName}.`);
+            console.error(error);
+
+            if(interaction.deferred || interaction.replied)
+            {
+                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+ 
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+
+
+    );
 
 
 
