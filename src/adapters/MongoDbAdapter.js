dd269ac895544550ab8f4ac4d21ae6f92779710a
Cenkarof <vergilicenkay@gmail.com>
2023-08-29 20:35:33 +0300

    mongodb insert with transaction

--- src/adapters/MongoDbAdapter.js
+++ src/adapters/MongoDbAdapter.js
@@ -8,65 +8,21 @@ const dbName = process.env.MONGODB_DBNAME;
 
 const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
-// async function main()
-// {
-//     const url = process.env.MONGODB_URL;
-//     const dbName = process.env.MONGODB_DBNAME;
-    
-//     const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
-    
-//     try
-//     {
-//         await client.connect();
-//         console.log('Connected to MongoDb');
-    
-//         const db = client.db(dbName);
-//         const collection = db.collection('user');
-    
-//         const user = new User({name:'Cenkay', discordUserId:'-', email:'test@gmail.com',experience:1,level:1,password:'strongpass'});
-    
-//         var result = await collection.insertOne(user);
-    
-//         console.log('Document inserted', result.insertedId);
-//     }
-//     catch(err)
-//     {
-//     console.error('Error',err);
-//     }
-//     finally
-//     {
-//     client.close();
-//     }
-// }
-
-//main();
-
-
-async function openConnection(collectionName)
+async function openConnection()
 {
     try{
         await client.connect();
-        const db = client.db(dbName);
-        const collections = await db.listCollections().toArray();
-    
-        const existingCollectionNames = collections.map((collection) => collection.name);
-    
-        if(!existingCollectionNames.includes(collectionName))
-        {
-            console.error(`[ERROR] Collection with name: ${collectionName} doesn't exist in the database.`);
-            await client.close();
-            return;
-        }
 
         const isConnected = client.topology.isConnected();
         if(isConnected)
         {
             console.log(`[INFO] Connection is established to collection: ${collectionName}`);
+            return true;
         }   
     }
     catch(err){
         console.error(`[ERROR] Connection could not be established. Error Message: ${err}`);
-        await client.close();
+        return false;
     }
 }
 
@@ -77,43 +33,56 @@ async function closeConnection()
 
         if(isConnected)
         {
-            const db = client.db(dbName);
-            const result = await db.command({currentOp: true});
-
-            const transactions = await result.inprog.filter((op) => op.txnNumber !== undefined);
-
-            if(transactions.length > 0)
-            {
-                let transactionsCount = transactions.length;
-
-                while(transactionsCount === 0)
-                {
-                    console.log(`[INFO] There are still open transactions left. Open transactions count: ${transactions.length}. Trying again in 3 seconds to finish up the transactions.`);
-
-                    setTimeout(()=>{
-                        let result = db.command({currentOp: true});
-                        let transactions = result.inprog.filter((op) => op.txnNumber !== undefined);
-    
-                        transactionsCount = transactions.length;
-                    },3000);
-                }
-            }
-
             console.log(`[INFO] Closing the connection.`);
+            await client.close();
         }
     }
     catch(err){
         console.error(`[ERROR] Error occured while checking the connection status. Error Message: ${err}`);
     }
-    finally{
-        await client.close();
-    }
 }
 
-async function waitForOpenTransactions(dbName)
+async function insertOneWithTransaction(entity)
 {
+    let collectionName;
+
+    if( entity instanceof User)
+        collectionName = 'user';
+
+    if(entity instanceof Log)
+        collectionName = 'log';
+
+    if(collectionName !== undefined)
+    {
+        try
+        {
+            const isConnectionEstablished = await openConnection();
+
+            if(isConnectionEstablished)
+            {
+                const db = client.db(dbName);
+                const collection = db.collection(collectionName);
     
+                const result = await collection.insertOne(entity);
+    
+                await closeConnection();
+    
+                console.log(`[INFO] A document has succesfully been inserted with the Id of ${result.insertedId}`);
+            }
+            else
+                console.error(`[ERROR] Couldn't establish connection`);
+        }
+        catch(err)
+        {
+            console.error(`[ERROR] An error occurred whilst trying to insert a record. Error Message: ${err}`);
+        }
+
+    }
+
+
 }
+
+
 
 (async()=>{
 await openConnection('user');
