14aca8f39f470c9d03d1bacce6a1fe61f45864f9
Cenkarof <vergilicenkay@gmail.com>
2023-09-10 00:24:18 +0300

    giphy search

--- src/services/calculation-helper-service.js
+++ src/services/calculation-helper-service.js
@@ -29,7 +29,7 @@ class CalculationHelper{
     
         return user;
     }
-}x
+}
 
 
 
--- src/services/fetchers/giphy-fetcher.js
+++ src/services/fetchers/giphy-fetcher.js
@@ -16,11 +16,11 @@ class GiphyFetcher{
         this.offset = offset;
     }
 
-    async trend(type)
+    async trend(type, overrideLimit=-1) 
     {
-        let query = {
+        const query = {
             api_key: this.api_key,
-            limit: this.limit,
+            limit: overrideLimit === -1 ? this.limit : overrideLimit,
             random_id: this.random_id,
             bundle: this.bundle,
             offset: this.offset
@@ -34,12 +34,43 @@ class GiphyFetcher{
         return await response.text();
     }
 
+    async search(type, queryKeywords, overrideLimit=-1, language='en')
+    {
+        const query = {
+            q:queryKeywords,
+            api_key: this.api_key,
+            limit: overrideLimit === -1 ? this.limit : overrideLimit,
+            random_id: this.random_id,
+            bundle: this.bundle,
+            offset: this.offset,
+            lang: language
+        };
+
+        const queryParams = querystring.stringify(query);
+
+        const apiUrl = this.base_url+`${type}/search?${queryParams}`;
+
+        const response = await fetch(apiUrl);
+        return await response.text();
+    }
+
+    async random(type, tag, overrideLimit=-1)
+    {
+        
+    }
+
+    async translate(type, searchTerm, weirdness, overrideLimit=-1)
+    {
+
+    }
+
 }
 
 
 (async ()=> {
-    let giphyFetcher = new GiphyFetcher('original',1,0);
-    let result = await giphyFetcher.trend(GiphyConsts.EndpointType.Gif);
+    let giphyFetcher = new GiphyFetcher('original',25,0);
+    // let result = await giphyFetcher.trend(GiphyConsts.EndpointType.Gif);
+    let result = await giphyFetcher.search(GiphyConsts.EndpointType.Gif, 'bleach');
     let parsedJson = JSON.parse(result);
     let data = JSON.stringify(parsedJson, null, 2);
     fs.writeFile('test-data.json', data, (err)=>{
